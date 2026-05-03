c7eeb1bd24307fa5bf9e0e884ba67143480cf736
sudu_493412 <sudarshanmohanchavan493412@gmail.com>
2026-05-04 01:03:43 +0530

    Completely re-arranged the hierarchy--added a
    base class for basic socket creation, and client
    
    and server children classes, with now support over
    
    LAN connection. Added bunch of logic for those features.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <stdint.h>
 #include <string.h>
 #include <sys/errno.h>
@@ -6,59 +7,170 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <netinet/in.h>
+#include <netdb.h>
 
 
-struct sockaddr_in my_addr;
+bool RunningMode; //1 for server. 0 for client
 
-class Connector{
-    private:
-        uint16_t port = 8000;
+void PrintServerInfo(uint16_t port){
+    char hostbuffer[256];
+
+    if (gethostname(hostbuffer, sizeof(hostbuffer)) < 0){
+        perror("gethostname");
+        return;
+    }
+
+    struct hostent* host_entry = gethostbyname(hostbuffer);
+    if(!host_entry){
+        perror("gethostbyname");
+        return;
+    }
+
+    char* ip = inet_ntoa(*((struct in_addr*) host_entry->h_addr));
+
+    printf("\n========Server Info========\n");
+    printf("Local IP: 127.0.0.1:%d\n", port);
+    printf("LAN IP: %s:%d\n", ip, port);
+    printf("===========================\n\n");
+}
+
+class BaseConnectionInstance{
+    protected:
         int sockfd;
 
     public:
-        Connector(){
+        BaseConnectionInstance(){
             sockfd = -1;
         }
 
         bool CreateSocketFd(){
-            sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
-            if(sockfd < 0){
-                perror("Socket");
-                return false;
-            }
-
-            printf("Socket creation successful.\n");
-            return true;
+           sockfd = socket(AF_INET, SOCK_STREAM, 0);
+           if(sockfd < 0){
+               perror("Socket");
+               return false;
+           }
+           printf("Socket creation successful.\n");
+           return true;
         }
 
         int GetFd(){
             return sockfd;
         }
 
-        void BindSocket(){
-            struct sockaddr_in serv_address;
-            serv_address.sin_family = AF_INET;
-            serv_address.sin_port = htons(port);
-            serv_address.sin_addr.s_addr = htonl(INADDR_ANY);
-
-            bind(sockfd, (struct sockaddr *) &serv_address, sizeof(serv_address));
-            printf("Socket bound to %d at port %d.\n", htons(INADDR_ANY), port);
-        }
-
-
-       ~Connector(){
-            if(sockfd >= 0){
+        virtual ~BaseConnectionInstance() noexcept{
+            if (sockfd >= 0){
                 close(sockfd);
-                printf("Socket closed.\n");
+            }
         }
-    }
+
 };
 
-int main(){
-    Connector newConnection;
-    int sockfd = newConnection.CreateSocketFd();
-    newConnection.BindSocket();
-    printf("The socket file descriptor is: %d.\n", sockfd);     
+class ServerInstance : public BaseConnectionInstance{
+    private:
+        uint16_t serv_port = 8000;
+
+    public:
+        int GetPort(){
+            return serv_port;
+        }
+
+        void StartListening(){
+            if (listen(sockfd, SOMAXCONN) < 0){
+                printf("Error: Couldn't listen.\n");
+            } else{
+                PrintServerInfo(serv_port);
+                printf("Listening on port %d...\n", serv_port);
+            }
+        }
+
+        int AcceptClient(){
+            struct sockaddr_in cli_addr{};
+            socklen_t cli_addr_len = sizeof(cli_addr);
+
+            int NewSockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &cli_addr_len);
+
+            if (NewSockfd < 0){
+                perror("accept");
+                return -1;
+            }
+
+            printf("Client is now connected.\n");
+            return NewSockfd;
+        }
+
+        void BindSocketToServer(){
+            struct sockaddr_in serv_address{};
+            serv_address.sin_family = AF_INET;
+            serv_address.sin_port = htons(serv_port);
+            serv_address.sin_addr.s_addr = htonl(INADDR_ANY);
+
+            int opt = 1;
+            setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+            if (bind(sockfd, (struct sockaddr *) &serv_address, sizeof(serv_address)) < 0){
+                perror("bind");
+                return;
+            }
+            
+            printf("Socket bound to 0.0.0.0 at port %d.\n", serv_port);
+        }
+
+
+        ~ServerInstance() noexcept override{}
+};
+
+class ClientInstance : public BaseConnectionInstance{
+    private:
+        uint16_t serv_port;
+
+    public:
+        void ConnectToServer(const char* ip, uint16_t port){
+            struct sockaddr_in serv_address{};
+            serv_address.sin_family = AF_INET;
+            serv_address.sin_port = htons(port);
+            serv_port = port;
+
+            if (inet_pton(AF_INET, ip, &serv_address.sin_addr) <= 0){
+                perror("inet_pton");
+                return;
+            };
+
+            if (connect(sockfd, (struct sockaddr *) &serv_address, sizeof(serv_address)) < 0){
+                perror("connect");
+                return;
+            }
+            
+            printf("Connected to %s:%d.\n", ip, port);
+        }
+
+        int GetPort(){
+            return serv_port;
+        }
+
+        ~ClientInstance()noexcept override{}
+};
+
+int main(int argc, char *argv[]){
+    if(argc < 2){
+        printf("Needs arguments.\n");
+        return -1;
+    }
+
+    if(strcmp(argv[1], "server") == 0){
+        RunningMode = true;
+        printf("Program running in server mode.\n");
+    } else if(strcmp(argv[1], "client") == 0){
+        RunningMode = false;
+        printf("Program running in client mode.\n");
+    } else {
+        printf("Invalid arguments.\n");
+    }
+
+    //SERVER MODE
+
+
+    //CLIENT MODE
+
+    return 0;
 }
 
