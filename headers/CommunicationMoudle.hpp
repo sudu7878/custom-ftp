adf87c255e83ca2224bfa61e985e21789b9181da
sudu_493412 <sudarshanmohanchavan493412@gmail.com>
2026-05-05 13:11:20 +0530

    Added some more stuff, in middle of creating a message packet body.s

--- headers/CommunicationMoudle.hpp
+++ headers/CommunicationMoudle.hpp
@@ -3,6 +3,14 @@
 #ifndef COMMUNICATION_MODULE
 #define COMMUNICATION_MODULE
 
+struct Packet{
+    uint8_t PL_TYPE;
+    uint16_t PL_LEN;
+    const char * PL_TXT;
+};
 
+int SizeOfMessagePacket(Packet& packet);
+
+bool SendPacket(int sockfd, Packet& packet, size_t size);
 
 #endif  /*COMMINCATION_MODULE*/
